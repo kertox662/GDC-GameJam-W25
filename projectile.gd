11277f1007d6c35c12aee75a76225cd4b302a46c
Misha Melnyk <as.k.github@melnyk.com>
2025-02-09 16:38:24 -0500

    push

--- projectile.gd
+++ projectile.gd
@@ -22,6 +22,7 @@ func _physics_process(delta):
 func _on_body_entered(body):
 	if body.get_class() == "TileMapLayer":
 		bounces -= 1
+		$BounceSound.pitch_scale = randf() + 1.2
 		$BounceSound.play(0)
 		if bounces == 0:
 			queue_free()
