1c7b4e15b3a83a28bac9583ae27fbad65dfb6753
Saaz Ramsubhag <ramsubhag.saaz@gmail.com>
2025-02-09 02:19:31 -0500

    added ui shortcuts and colored player select

--- game/start/player_join_block.gd
+++ game/start/player_join_block.gd
@@ -1,21 +1,31 @@
-extends Control
+extends PanelContainer
 
 const DEFAULT_TEXT = "Space / A / X"
 
 @export var playerIcon: Texture
+@export var blockColor: Color  # color when joined
 
 var inputDevice: InputDevice = null
+
+var blankColor: Color
+var styleBox: StyleBoxFlat
 
 func _ready():
 	$MarginContainer/VBoxContainer/TextureRect.texture = playerIcon
 	$MarginContainer/VBoxContainer/Button.custom_minimum_size = $MarginContainer/VBoxContainer/Button.size
+	styleBox = get_theme_stylebox("panel").duplicate()
+	blankColor = styleBox.bg_color
 
 func set_player_joined(device: InputDevice, name: String):
 	inputDevice = device
+	styleBox.bg_color = blockColor
+	add_theme_stylebox_override("panel", styleBox)
 	$MarginContainer/VBoxContainer/Button.text = name
 
 func set_player_left():
 	inputDevice = null
+	styleBox.bg_color = blankColor
+	add_theme_stylebox_override("panel", styleBox)
 	$MarginContainer/VBoxContainer/Button.text = DEFAULT_TEXT
 
 func get_player_data():
--- game/start/start_screen.gd
+++ game/start/start_screen.gd
@@ -1,5 +1,8 @@
 extends Control
 
+func _ready() -> void:
+	$ContentMargins/ContentVbox/ButtonHBox/Start.grab_focus()
+
 func _on_start_button_down() -> void:
 	get_tree().change_scene_to_file("res://game/start/pre_match.tscn")
 
