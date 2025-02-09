ae40b7be86a89fae4a2d8ab6ea1e4f11e05f6012
Misha Melnyk <as.k.github@melnyk.com>
2025-02-09 16:19:35 -0500

    credits screen

--- game/start/start_screen.gd
+++ game/start/start_screen.gd
@@ -6,6 +6,11 @@ func _ready() -> void:
 func _on_start_button_down() -> void:
 	get_tree().change_scene_to_file("res://game/start/pre_match.tscn")
 
-
 func _on_quit_button_down() -> void:
 	get_tree().quit()
+
+func _on_credits_button_down() -> void:
+	$Credits.visible = !$Credits.visible
+
+func _on_back_to_start_button_down() -> void:
+	$Credits.visible = !$Credits.visible
